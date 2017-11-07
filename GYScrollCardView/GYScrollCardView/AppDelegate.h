cc850a8d2d04384c6d4596196f81e2e7c742b0f8
gy_lyons <gy_lyons@163.com>
2017-11-07 10:16:46 +0800

    Commit first

---
+++ GYScrollCardView/GYScrollCardView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GYScrollCardView
+//
+//  Created by lyons on 2017/11/7.
+//  Copyright © 2017年 GY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GYScrollCardView/GYScrollCardView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GYScrollCardView
+//
+//  Created by lyons on 2017/11/7.
+//  Copyright © 2017年 GY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
